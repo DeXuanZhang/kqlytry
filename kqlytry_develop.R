e8574ea55ba5a6e3ddbb0f6546aa48265d0967d2
Dexuan Zhang <zdxssss@126.com>
2021-11-22 00:50:35 -0600

    Update Description

--- kqlytry_develop.R
+++ kqlytry_develop.R
@@ -1,5 +1,9 @@
 library(devtools)
+library(desc)
 create_package("../kqlytry")
 
 use_git()
 use_github()
+desc_add_author(given = "Dexuan",family = "Zhang",email = "dezhang@mcw.edu",
+                role=c("aut","cre"))
+use_gpl3_license()
